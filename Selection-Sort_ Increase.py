d0f6967128f3ff749650ccf875448c710a40e9a8
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-11-29 21:46:31 +0530

    Add selection sort implementation in Python
    Implement selection sort algorithm to sort a list of numbers.

---
+++ Selection-Sort_ Increase.py
@@ -0,0 +1,14 @@
+class Solution:
+    def selectionSort(self, nums):
+        for i in range(len(nums)):
+            minV =i 
+            for j in range(len(nums)-1):
+                if nums[i]<nums[j]:
+                    nums[minV],nums[j] = nums[j],nums[minV]
+        return nums
+n=input().strip("[]")
+n=list(map(int,n.split(",")))
+print(Solution().selectionSort(n))
+
+#Input: nums = [7, 4, 1, 5, 3]
+#Output: [1, 3, 4, 5, 7]
