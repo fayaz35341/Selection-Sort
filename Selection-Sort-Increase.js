f8eb9435c453ac9a372613ba71b6a3ebf3e5d591
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2025-11-29 22:32:26 +0530

    Create Selection-Sort-Increase.js

---
+++ Selection-Sort-Increase.js
@@ -0,0 +1,16 @@
+class Solution {
+    selectionSort(nums) {
+        for (let i = 0; i < nums.length; i++) {
+            let minIndex = i;
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[j] <nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
+        }
+        return nums;
+    }
+}
+const u = [7, 4, 1, 5, 3];
+console.log(new Solution().selectionSort(u));
